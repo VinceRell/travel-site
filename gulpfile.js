a8ff37932a353f906935f5076595a1314b2912ae
Vincent <v.rell_home@outlook.com>
2017-08-01 03:46:22 +0200

    added more postcss plugins

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,22 @@
 var gulp = require("gulp"),
-watch = require("gulp-watch");
+watch = require("gulp-watch"),
+postcss = require("postcss"),
+autoPrefixer = require("autoprefixer"),
+simpleVars = require("postcss-simple-vars"),
+nested = require("postcss-nested");
 
 gulp.task("default", function(){
   console.log("this is a test");
 });
 
+gulp.task("styles", function(){
+  return gulp.src("./app/assets/styles/styles.css")
+  .pipe(postcss([nested, simpleVars, autoPrefixer]))
+  .pipe(gulp.dest("./app/temp/styles"));
+});
+
 gulp.task("watch", function(){
-   watch("./test.html", function(){
-      gulp.start("default");
+   watch("./app/assets/**/*.css", function(){
+      gulp.start("styles");
    });
 });

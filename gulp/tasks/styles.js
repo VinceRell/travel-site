eb4f4d9447708df7327784cfd38315818882c6f4
Vincent <v.rell_home@outlook.com>
2017-09-18 00:57:36 +0200

    added styling to navbar

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -4,11 +4,12 @@ autoPrefixer = require("autoprefixer"),
 cssvars = require("postcss-simple-vars"),
 nested = require("postcss-nested"),
 cssImport = require("postcss-import"),
-mixins = require("postcss-mixins");
+mixins = require("postcss-mixins"),
+hexrgba = require("postcss-hexrgba");
 
 gulp.task("styles", function(){
   return gulp.src("./app/assets/styles/styles.css")
-  .pipe(postcss([cssImport,mixins,nested,cssvars,autoPrefixer]))
+  .pipe(postcss([cssImport,mixins,nested,cssvars,hexrgba,autoPrefixer]))
   .on("error",function(errorInfo){
     console.log(errorInfo);
     this.emit("end");
